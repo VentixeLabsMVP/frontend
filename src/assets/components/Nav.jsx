5d0c389a15aa3413e0266fc370826b9b6bdbe9c5
fredrik <freedrik92@hotmail.com>
2025-05-20 16:34:17 +0200

    added styling to links

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../images/logo_ventixe.svg'
-
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
@@ -9,6 +9,14 @@ const Nav = () => {
         <img className="logo-img" src={logo} alt="Ventixe logo"/>
         <span className="logo-text">Ventixe</span>
       </div>
+      <NavLink to="/" end className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
+        <i className="fa-solid fa-ticket nav-icon"></i>
+        <span className="nav-text">Events</span>
+      </NavLink>
+      <NavLink to="/dummy" end className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}>
+        <i className="fa-solid fa-ticket nav-icon"></i>
+        <span className="nav-text">Not Events</span>
+      </NavLink>
     </nav>
   )
 }
